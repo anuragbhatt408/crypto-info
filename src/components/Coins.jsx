dfb04daa48d6b9e2014942f7a5e1c821303a7444
anurag bhatt <anuragbhatt408@gmail.com>
2022-12-17 12:56:17 +0530

    cypto info

---
+++ src/components/Coins.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "..";
+import { Container, HStack, RadioGroup, Radio, Button } from "@chakra-ui/react";
+import ErrorComponent from "./ErrorComponent";
+import Loader from "./Loader";
+import CoinsCard from "./CoinsCard";
+
+const Coins = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/coins/markets?vs_currency=${currency}&page=${page}`
+        );
+        setCoins(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency, page]);
+
+  if (error) return <ErrorComponent message={"Error While Fetching Coins"} />;
+
+  const changePage = (index) => {
+    setPage(index);
+    setLoading(true);
+  };
+
+  const btns = new Array(132).fill(1);
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>USD</Radio>
+              <Radio value={"eur"}>EUR</Radio>
+            </HStack>
+          </RadioGroup>
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+            {coins.map((i) => (
+              <CoinsCard
+                id={i.id}
+                key={i.id}
+                name={i.name}
+                symbol={i.symbol}
+                img={i.image}
+                price={i.current_price}
+                currencySymbol={currencySymbol}
+              />
+            ))}
+          </HStack>
+          <HStack w={"full"} overflowX={"auto"} p={"8"}>
+            {btns.map((item, index) => (
+              <Button
+                key={index}
+                bgColor={"blackAlpha.900"}
+                color={"white"}
+                onClick={() => changePage(index + 1)}
+                sx={{
+                  "&:hover": {
+                    color: "black",
+                    bgColor: "grey",
+                  },
+                }}
+              >
+                {index + 1}
+              </Button>
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default Coins;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc =
+  "https://avatars.githubusercontent.com/u/53056656?s=400&u=64aab700a7f9d0ac7e3ebdb6445807e357091331&v=4";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            Find interesting information about crypto
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Our Founder</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
